68d8d33c2a10a8297b0f0d3fe8a9195b74779585
joynto7 <joyntoghosh7@gmail.com>
2025-10-27 22:40:39 +0600

    Loops in Javascript

---
+++ loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    std::cout <<"starting loop example" << std::endl;
+    for (int i=0; i<5; i++){
+        std::cout <<"Iteration number:"<< i << std::endl;
+        
+    }
+    std:: cout <<"Loop Ended" << std::endl;
+    return 0;
+
+}
