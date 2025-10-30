0b7b2c65b8366898e01c64d4b728d2e50181f58f
joynto7 <joyntoghosh7@gmail.com>
2025-10-30 12:38:04 +0600

    if...else

---
+++ if_else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    std::cout << "Enter the first integer:";
+    std::cin >> a;
+    int b;
+    std::cout << "Enter the second integer:";
+    std:cin >> b;
+
+    if (a>b){
+        std::cout << a   << " " <<  "is greater than"<< " " << b << std::endl;
+    }else{
+        std::cout << a << " " << "is smaller than" <<" " << b << std :: endl;
+    }
+
+}
